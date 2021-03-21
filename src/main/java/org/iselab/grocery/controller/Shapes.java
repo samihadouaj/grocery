d758ce53d1b9849bf8c81daf6734d6e559150c3d
Sami Hadouaj <samihadouaj@Samis-MacBook-Pro.local>
2021-03-21 10:07:51 +0100

    created shape abstract class and circle is extending it

--- src/main/java/org/iselab/grocery/controller/Shapes.java
+++ src/main/java/org/iselab/grocery/controller/Shapes.java
@@ -1,27 +1,17 @@
 package org.iselab.grocery.controller;
 
-public class Shapes {
-	public int calculateShape(String shape) {
-		if(shape =="cercle") {
-			System.out.println("area is blab;la");
-		}
-		else if(shape =="rectangle") {
-			System.out.println("area is fsadfa;la");
-		}
-		
-		else if(shape =="TRIANGLE") {
-			System.out.println("area is FDSAASF;la");
-		}
-		
-		else if(shape =="ellipse") {
-			System.out.println("area is gdsadfsa;la");
-		}
-		else {
-			return 5;
-		}
-		return 0;
-		
+public abstract class Shapes {
+    String shape;
+
+    public Shapes(String shape) {
+        this.shape = shape;
+    }
+
+    public void  getShape() {
+		System.out.println("this is a "+ this.shape);
 	}
-	
+	public abstract double calculateArea(double d);
+
+
 
 }
